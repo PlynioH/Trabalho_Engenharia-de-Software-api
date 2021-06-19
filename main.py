d8456baba04ade5020adf1a6a1687462b9212da7
Plynio Henrique Murat da Silva <66133284+PlynioH@users.noreply.github.com>
2021-06-18 21:58:56 -0300

    cadastro leito

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from features.cadastro_leito.cadastro_leito import CadastroLeito
 from features.login.login import Login
 from features.cadastro_user.cadastro_user import CadastroUser
 from flask import Flask
@@ -10,6 +11,7 @@ api = Api(app)
 api.add_resource(On, '/')
 api.add_resource(CadastroUser, '/cadastro/user')
 api.add_resource(Login, '/login')
+api.add_resource(CadastroLeito, '/cadastro/leito')
 
 if __name__ == '__main__':
     app.run()
